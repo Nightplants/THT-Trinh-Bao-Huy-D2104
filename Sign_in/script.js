74633426ae5af2560d2d0b6e258ad99fc4d1edbd
Nightplants <trinhbaohuy14@gmail.com>
2026-03-02 19:51:15 +0700

    commit

--- Sign_in/script.js
+++ Sign_in/script.js
@@ -11,11 +11,28 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  databaseURL: "YOUR_DB_URL",
-  projectId: "YOUR_ID",
+  apiKey: "AIzaSyAuex480J0Xcxvdj-nSCd61ZtgapNf3fXI",
+  authDomain: "tht-trinh-bao-huy-d2104.firebaseapp.com",
+  databaseURL: "https://tht-trinh-bao-huy-d2104-default-rtdb.firebaseio.com",
+  projectId: "tht-trinh-bao-huy-d2104",
+  storageBucket: "tht-trinh-bao-huy-d2104.firebasestorage.app",
+  messagingSenderId: "267969297800",
+  appId: "1:267969297800:web:9c68370e626f0cb2b3ce3d",
+  measurementId: "G-V8E9HP76SX",
 };
+
+function validEmail(email) {
+  return email.includes("@") && email.includes(".com");
+}
+
+function validPassword(password) {
+  let lengthValid = password.length > 8 && password.length < 30;
+  let hasLetter = /[a-zA-Z]/.test(password);
+  let hasNumber = /[0-9]/.test(password);
+  let hasSpecial = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+  return lengthValid && hasLetter && hasNumber && hasSpecial;
+}
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -25,7 +42,17 @@ window.login = function () {
   let input = document.getElementById("loginInput").value;
   let password = document.getElementById("password").value;
 
+  if (!validPassword(password)) {
+    alert("Mật khẩu không hợp lệ!");
+    return;
+  }
+
   if (input.includes("@")) {
+    if (!validEmail(input)) {
+      alert("Email phải chứa @ và .com");
+      return;
+    }
+
     signInWithEmailAndPassword(auth, input, password)
       .then(() => alert("Đăng nhập thành công!"))
       .catch(() => alert("Sai thông tin!"));
--- Sign_up/script.js
+++ Sign_up/script.js
@@ -7,43 +7,76 @@ import {
   getDatabase,
   ref,
   set,
+  get,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  databaseURL: "YOUR_DB_URL",
-  projectId: "YOUR_ID",
+  apiKey: "AIzaSyAuex480J0Xcxvdj-nSCd61ZtgapNf3fXI",
+  authDomain: "tht-trinh-bao-huy-d2104.firebaseapp.com",
+  databaseURL: "https://tht-trinh-bao-huy-d2104-default-rtdb.firebaseio.com",
+  projectId: "tht-trinh-bao-huy-d2104",
+  storageBucket: "tht-trinh-bao-huy-d2104.firebasestorage.app",
+  messagingSenderId: "267969297800",
+  appId: "1:267969297800:web:9c68370e626f0cb2b3ce3d",
+  measurementId: "G-V8E9HP76SX",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 
-window.register = function () {
-  let username = document.getElementById("username").value;
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
-  let confirmPassword = document.getElementById("confirmPassword").value;
+function validEmail(email) {
+  return email.includes("@") && email.includes(".com");
+}
 
-  if (password !== confirmPassword) {
-    alert("Mật khẩu không khớp!");
+function validPassword(password) {
+  let lengthValid = password.length > 8 && password.length < 30;
+  let hasLetter = /[a-zA-Z]/.test(password);
+  let hasNumber = /[0-9]/.test(password);
+  let hasSpecial = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+  return lengthValid && hasLetter && hasNumber && hasSpecial;
+}
+
+window.register = function () {
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const repassword = document.getElementById("confirmPassword").value;
+
+  if (!email.includes("@") || !email.includes(".com")) {
+    alert("Email phải có @ và .com");
+    return;
+  }
+
+  const passRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[\W_]).{8,30}$/;
+
+  if (!passRegex.test(password)) {
+    alert("Password phải có chữ, số, ký tự đặc biệt và 8-30 ký tự");
+    return;
+  }
+
+  if (password !== repassword) {
+    alert("Mật khẩu nhập lại không khớp");
     return;
   }
 
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      let user = userCredential.user;
+      const user = userCredential.user;
 
       set(ref(db, "users/" + user.uid), {
         username: username,
         email: email,
+        password: password,
+      }).then(() => {
+        alert("Đăng ký thành công!");
+        window.location.href = "../Sign_in/index.html";
       });
-
-      alert("Tạo tài khoản thành công!");
-      window.location.href = "../Sign_in/index.html";
     })
-    .catch((error) => alert(error.message));
+    .catch((error) => {
+      alert(error.message);
+    });
 };
 
 window.togglePass = function (id) {
