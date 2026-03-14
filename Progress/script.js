7ef850261573f430ded72399d555992dfdb1c913
Nightplants <trinhbaohuy14@gmail.com>
2026-03-14 21:57:07 +0700

    commit

--- Progress/script.js
+++ Progress/script.js
@@ -5,8 +5,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import {
   getDatabase,
   ref,
-  get,
-  set,
+  get
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
@@ -38,27 +37,100 @@ function loadProfile() {
 }
 
 function loadDreams() {
+
   let userId = localStorage.getItem("userId");
 
   get(ref(db, "users/" + userId + "/dreams")).then((snapshot) => {
+
     let container = document.getElementById("dreamContainer");
     container.innerHTML = "";
 
+    let index = 0;
+
     snapshot.forEach((child) => {
+
       let dream = child.val();
 
+      let steps = dream.steps ? dream.steps.split(",") : [];
+
       let html = `
-<div class="dream">
-<div class="dreamTitle">
-${dream.name}
-<span>0%</span>
-</div>
-</div>
-`;
+      <div class="dream">
+
+        <div class="dreamTitle" onclick="toggleSteps(${index})">
+          ${dream.name}
+          <span id="percent-${index}">0%</span>
+        </div>
+
+        <div class="steps" id="steps-${index}">
+      `;
+
+      steps.forEach(step => {
+
+        html += `
+        <label>
+          <input type="checkbox" onchange="updateProgress(${index})">
+          ${step}
+        </label>
+        `;
+
+      });
+
+      html += `
+          <div class="progressBar">
+            <div class="progress" id="bar-${index}"></div>
+          </div>
+        </div>
+
+      </div>
+      `;
+
       container.innerHTML += html;
+
+      index++;
+
     });
+
   });
+
+}
+
+function toggleSteps(index) {
+
+  let steps = document.getElementById("steps-" + index);
+
+  if (steps.style.display === "block") {
+    steps.style.display = "none";
+  } else {
+    steps.style.display = "block";
+  }
+
+}
+
+function updateProgress(index) {
+
+  let checkboxes = document.querySelectorAll(`#steps-${index} input`);
+
+  let total = checkboxes.length;
+  let done = 0;
+
+  checkboxes.forEach(cb => {
+    if (cb.checked) done++;
+  });
+
+  let percent = 0;
+
+  if (total > 0) {
+    percent = Math.round(done / total * 100);
+  }
+
+  document.getElementById("percent-" + index).innerText = percent + "%";
+
+  document.getElementById("bar-" + index).style.width = percent + "%";
+
 }
 
 loadDreams();
+
 window.loadProfile = loadProfile;
+window.toggleSteps = toggleSteps;
+window.updateProgress = updateProgress;
