c28420094d02cc9f3e3dc42c07a4c82cbc1fb8dd
Nightplants <trinhbaohuy14@gmail.com>
2026-03-29 08:12:21 +0700

    commit

--- Home/script.js
+++ Home/script.js
@@ -1,4 +1,26 @@
-function loadProfile() {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuex480J0Xcxvdj-nSCd61ZtgapNf3fXI",
+  authDomain: "tht-trinh-bao-huy-d2104.firebaseapp.com",
+  databaseURL: "https://tht-trinh-bao-huy-d2104-default-rtdb.firebaseio.com",
+  projectId: "tht-trinh-bao-huy-d2104",
+  storageBucket: "tht-trinh-bao-huy-d2104.firebasestorage.app",
+  messagingSenderId: "267969297800",
+  appId: "1:267969297800:web:9c68370e626f0cb2b3ce3d",
+  measurementId: "G-V8E9HP76SX",
+};
+
+window.loadProfile = function () {
   let name = localStorage.getItem("username");
   let profile = localStorage.getItem("profile");
 
@@ -9,22 +31,12 @@ function loadProfile() {
   if (profile) {
     document.getElementById("profile").src = profile;
   }
-}
+};
 
-function goProfile() {
+window.goProfile = function () {
   window.location.href = "../Profile/index.html";
-}
+};
 
-async function requestNotificationPermission() {
-  if (!("Notification" in window)) {
-    console.log("Trình duyệt này không hỗ trợ thông báo.");
-    return;
-  }
-
-  if (Notification.permission === "default") {
-    const permission = await Notification.requestPermission();
-    if (permission === "granted") {
-      console.log("Đã được cấp quyền!");
-    }
-  }
-}
+window.goDreams = function () {
+  window.location.href = "../Dreams/index.html";
+};
